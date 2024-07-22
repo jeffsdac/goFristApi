c944856787fcfd681fd27e5699954653bf9d8c5f
jeffsdac <contato.jeffsdac@gmail.com>
2024-07-22 09:24:39 -0300

    FEAT: ping test

--- api/ping.go
+++ api/ping.go
@@ -5,9 +5,6 @@ import (
 	"net/http"
 )
 
-func testPing(writer http.ResponseWriter, request *http.Request) {
-	if request.Method == http.MethodGet {
-		fmt.Fprintf(writer, "Ping realizado com sucesso! method GET")
-	}
+func testPing(writer http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(writer, "Realize seu ping com o method GET!")
 }
